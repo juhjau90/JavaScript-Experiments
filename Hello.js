3c86c3a4e87da1e67539a9735ac853a9a6154f1f
Juho Jauhiainen <j.jauhiainen@surffi.net>
2018-10-01 20:58:06 +0300

    Pvm 01.10.2018 - Luotu tietovarasto JavaScript kokeiluille. Toistaiseksi vain yksinkertaista tavaraa muistin virkistämiseksi. Date 01.10.2018 - Repository created for JavaScript experiments. So far just some simple stuff to refresh memory.

---
+++ Hello.js
@@ -0,0 +1,2 @@
+/*Tiny Javascript to display a message. Pieni Javascriptin viestin näyttämiseen.*/
+alert("Hello World! This message brought to you by Javascript.");
---
+++ helloNode.js
@@ -0,0 +1,4 @@
+/*Just a simpple "hello" message for confirming Node.js is succesfully installed.
+Ihan simppeli "Hello" viesti varmistamaan että Node.js on asennettu onnistuneesti.*/
+
+console.log('Node is up and running!');
